d0d07b282456679b2ae1cfaf3773b0b8606a60e9
Adam McNeilly <amcneilly331@gmail.com>
2024-08-27 21:10:58 -0400

    Updating pager indicator.

--- app/src/main/java/com/adammcneilly/alltrails/ATPagerIndicator.kt
+++ app/src/main/java/com/adammcneilly/alltrails/ATPagerIndicator.kt
@@ -1,19 +1,19 @@
 package com.adammcneilly.alltrails
 
-import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.adammcneilly.alltrails.theme.AllTrailsTheme
 
@@ -25,43 +25,122 @@ fun ATPagerIndicator(
 ) {
     val selectedColor = Color.White
     val defaultColor = selectedColor.copy(
-        alpha = 0.33F,
+        alpha = 0.50F,
     )
 
+    val defaultSize = 12.dp
+    val miniSize = 8.dp
+
     Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier,
     ) {
-        repeat(totalPages) { index ->
-            val color = if (currentPageIndex == index) {
-                selectedColor
-            } else {
-                defaultColor
-            }
+        StartIndicator(
+            currentPageIndex = currentPageIndex,
+            selectedColor = selectedColor,
+            defaultColor = defaultColor,
+            defaultSize = defaultSize,
+            miniSize = miniSize,
+        )
 
-            Box(
-                modifier = Modifier
-                    .padding(2.dp)
-                    .clip(CircleShape)
-                    .background(color)
-                    .size(16.dp),
-            )
-        }
+        MidIndicator(
+            currentPageIndex = currentPageIndex,
+            totalPages = totalPages,
+            selectedColor = selectedColor,
+            defaultColor = defaultColor,
+            defaultSize = defaultSize,
+        )
+
+        EndIndicator(
+            currentPageIndex = currentPageIndex,
+            totalPages = totalPages,
+            selectedColor = selectedColor,
+            defaultColor = defaultColor,
+            defaultSize = defaultSize,
+            miniSize = miniSize,
+        )
     }
 }
 
-@Preview(
-    name = "Day Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-)
-@Preview(
-    name = "Night Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-)
+@Composable
+private fun EndIndicator(
+    currentPageIndex: Int,
+    totalPages: Int,
+    selectedColor: Color,
+    defaultColor: Color,
+    defaultSize: Dp,
+    miniSize: Dp,
+) {
+    val endIndicatorColor = when {
+        currentPageIndex == totalPages - 1 -> selectedColor
+        else -> defaultColor
+    }
+
+    val endIndicatorSize = when {
+        currentPageIndex >= totalPages - 2 -> defaultSize
+        else -> miniSize
+    }
+
+    IndicatorDot(endIndicatorColor, endIndicatorSize)
+}
+
+@Composable
+private fun MidIndicator(
+    currentPageIndex: Int,
+    totalPages: Int,
+    selectedColor: Color,
+    defaultColor: Color,
+    defaultSize: Dp,
+) {
+    val midIndicatorColor = when {
+        currentPageIndex > 0 && currentPageIndex < totalPages - 1 -> selectedColor
+        else -> defaultColor
+    }
+
+    IndicatorDot(midIndicatorColor, defaultSize)
+}
+
+@Composable
+private fun StartIndicator(
+    currentPageIndex: Int,
+    selectedColor: Color,
+    defaultColor: Color,
+    defaultSize: Dp,
+    miniSize: Dp,
+) {
+    val startIndicatorColor = when {
+        currentPageIndex == 0 -> selectedColor
+        else -> defaultColor
+    }
+
+    val startIndicatorSize = when {
+        currentPageIndex <= 1 -> defaultSize
+        else -> miniSize
+    }
+
+    IndicatorDot(startIndicatorColor, startIndicatorSize)
+}
+
+@Composable
+private fun IndicatorDot(
+    color: Color,
+    size: Dp,
+) {
+    Box(
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(color)
+            .size(size),
+    )
+}
+
+@Preview
 @Composable
 private fun StartPagerIndicatorPreview() {
     AllTrailsTheme {
         Surface(
-            color = MaterialTheme.colorScheme.background,
+            color = Color.Black,
         ) {
             ATPagerIndicator(
                 currentPageIndex = 0,
@@ -71,19 +150,12 @@ private fun StartPagerIndicatorPreview() {
     }
 }
 
-@Preview(
-    name = "Day Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-)
-@Preview(
-    name = "Night Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-)
+@Preview
 @Composable
 private fun MiddlePagerIndicatorPreview() {
     AllTrailsTheme {
         Surface(
-            color = MaterialTheme.colorScheme.background,
+            color = Color.Black,
         ) {
             ATPagerIndicator(
                 currentPageIndex = 2,
@@ -93,19 +165,12 @@ private fun MiddlePagerIndicatorPreview() {
     }
 }
 
-@Preview(
-    name = "Day Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-)
-@Preview(
-    name = "Night Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-)
+@Preview
 @Composable
 private fun EndPagerIndicatorPreview() {
     AllTrailsTheme {
         Surface(
-            color = MaterialTheme.colorScheme.background,
+            color = Color.Black,
         ) {
             ATPagerIndicator(
                 currentPageIndex = 4,
