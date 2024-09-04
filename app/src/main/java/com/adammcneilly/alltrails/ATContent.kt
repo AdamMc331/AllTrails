b9b3c110e6d10a5a9c1bda94be51e26e728ef530
Adam McNeilly <amcneilly331@gmail.com>
2024-09-04 19:24:12 -0400

    Adding remember call.

--- app/src/main/java/com/adammcneilly/alltrails/ATContent.kt
+++ app/src/main/java/com/adammcneilly/alltrails/ATContent.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -69,8 +70,10 @@ private fun TrailList(modifier: Modifier = Modifier) {
             .fillMaxWidth(),
     ) {
         val listState = rememberLazyListState()
-        val firstVisibleItemIndex = derivedStateOf {
-            listState.firstVisibleItemScrollOffset
+        val firstVisibleItemIndex = remember {
+            derivedStateOf {
+                listState.firstVisibleItemScrollOffset
+            }
         }
 
         LazyColumn(
