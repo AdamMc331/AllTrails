a441092b5820e4491f5de5bc9db659c62daab9e8
Adam McNeilly <amcneilly331@gmail.com>
2024-09-04 20:25:34 -0400

    Animating search input placeholder. (#13)

--- app/src/main/java/com/adammcneilly/alltrails/ATSearchInput.kt
+++ app/src/main/java/com/adammcneilly/alltrails/ATSearchInput.kt
@@ -2,6 +2,7 @@ package com.adammcneilly.alltrails
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
@@ -12,11 +13,18 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.adammcneilly.alltrails.theme.AllTrailsTheme
+import kotlinx.coroutines.delay
 
 /**
  * This is implemented as a [SearchBar] for now to get
@@ -29,8 +37,22 @@ import com.adammcneilly.alltrails.theme.AllTrailsTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ATSearchInput(modifier: Modifier = Modifier) {
+    val typeOptions = listOf("trails", "parks", "cities")
+
+    var typeIndex by remember {
+        mutableStateOf(0)
+    }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(3000)
+
+            typeIndex = (typeIndex + 1) % typeOptions.size
+        }
+    }
+
     SearchBar(
-        query = "Find trails",
+        query = "",
         onQueryChange = {},
         onSearch = {},
         active = false,
@@ -51,6 +73,17 @@ fun ATSearchInput(modifier: Modifier = Modifier) {
                         shape = CircleShape,
                     ).padding(6.dp),
             )
+        },
+        placeholder = {
+            Row {
+                Text(
+                    text = "Find ",
+                )
+
+                ScrollingText(
+                    text = typeOptions[typeIndex],
+                )
+            }
         },
         modifier = modifier,
     ) {
